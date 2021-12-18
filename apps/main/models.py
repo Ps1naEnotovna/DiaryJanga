b13be4a19c4876af6eddf66f7887dc5c7d89e733
German Goltsov <9150654-GerGoltz@users.noreply.gitlab.com>
2021-12-18 13:15:55 +0300

    cudate improvemetns

--- apps/main/models.py
+++ apps/main/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from ..accordant_utils.base_model import CUDateModel
 
 
-class Announcement(models.Model, CUDateModel):
+class Announcement(CUDateModel):
     name = models.CharField(max_length=255)
     text = models.TextField()
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'announcement'

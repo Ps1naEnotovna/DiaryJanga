44617db3dfc08a280e153aa68f23e805670f34e2
Ps1naEnotovna <salnikovasofa01@gmail.com>
2022-01-07 14:53:47 +1000

    ia ovosh'

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 from ..accordant_utils.base_model import CUDateModel
 
@@ -12,3 +13,56 @@ class Announcement(CUDateModel):
 
     class Meta:
         db_table = 'announcement'
+        verbose_name = 'объявление'
+        verbose_name_plural = 'Объявления'
+
+
+class Student(CUDateModel):
+    name = models.CharField('Имя', max_length=32)
+    last_name = models.CharField('Фамилия', max_length=32)
+    training = models.ForeignKey(Training, verbose_name='Поток',
+                                 on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f'{self.name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
+
+    class Meta:
+        db_table = 'student'
+        verbose_name = 'ученик'
+        verbose_name_plural = 'Ученики'
+
+
+class Subject(models.Model):
+    name = models.CharField('Название', max_length=32)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'subjects'
+        verbose_name = 'предмет'
+        verbose_name_plural = 'Предметы'
+
+
+class Homework(CUDateModel):
+    homework = models.TextField('Домашнее задание')
+    subject = models.ForeignKey(Subject, verbose_name='Предмет',
+                                on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.homework
+
+    class Meta:
+        db_table = 'homework'
+        verbose_name = 'домашнее задание'
+        verbose_name_plural = 'Домашнее задания'
+
+
+class Schedule(models.Model):
+    student = models.ForeignKey(Student, verbose_name='Ученик')
+    subject = models.ForeignKey(Subject, verbose_name='Предмет',
+                                on_delete=models.CASCADE, null=True)
+    date = models.DateField('Дата', default=now)
